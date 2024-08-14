8ccfb2838bfb703927565acce01d21a2124a4b05
mitsuun <lounge.attachment@gmail.com>
2024-08-14 13:47:40 +0900

    first commit

---
+++ src/app/api/tasks/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import { resolve } from "path";
+
+export interface Task {
+  id: number;
+  name: string;
+}
+
+const tasks: Task[] = [
+  { id: 1, name: 'プログラミング' },
+  { id: 2, name: 'ランニング' },
+];
+
+const sleep = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const GET = async () => {
+  await sleep(3000);
+  return NextResponse.json({ tasks }, { status: 200 })
+};
+
+export const dynamic = 'force-dynamic';
---
+++ src/app/sc/page.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const ServerComponent = () => {
+  console.log('Server Component');
+  // useState()
+  return (
+    <div>ServerComponent</div>
+  )
+}
+
+export default ServerComponent
